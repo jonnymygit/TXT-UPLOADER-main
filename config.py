62eace3a1273eb62d99ac71fc9d07f1fbc3714df
jonnymygit <jonnyjames9022@gmail.com>
2026-02-09 11:03:16 +0530

    Update default ADMINS environment variable value

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ LOG = ,
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "8342427239").split()):
+    for x in (os.environ.get("ADMINS", "7192966134").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
